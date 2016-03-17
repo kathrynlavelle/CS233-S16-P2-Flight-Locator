77895fa2ed0585f07b08d4ac277574fd22edc8c3
Kathryn <kathrynlavelle@kings.edu>
2016-03-17 19:08:32 -0400

    Working on reading in index files.

--- CS233 Project 2/src/edu/kings/cs233/proxy/FlightLocator.java
+++ CS233 Project 2/src/edu/kings/cs233/proxy/FlightLocator.java
@@ -1,8 +1,11 @@
 package edu.kings.cs233.proxy;
 
 import java.awt.List;
+import java.io.RandomAccessFile;
 
 import edu.kings.cs.util.Map;
+import edu.kings.cs233.hw.indexfile.DatumMock;
+import edu.kings.cs233.hw.indexfile.ListMap;
 
 /**
  * 
@@ -14,10 +17,18 @@ public class FlightLocator {
 	private ListMap<Integer, FlightRecord> flightData;
 	
 	/** Map of departure times to a list of FlightRecords. */
-	private ListMap<Long, List> departData;
+	private ListMap<Long, List<FlightRecord>> departData;
 	
 	/** Map of destinations to a list of FlightRecords. */
-	private ListMap<String, List> destData;
+	private ListMap<String, List<FlightRecord>> destData;
+	
+	/** File where all the records are stored. */
+	private RandomAccessFile flights;
+	
+	/** Index files. */
+	private RandomAccessFile flightNum;
+	private RandomAccessFile departTime;
+	private RandomAccessFile destination;
 
 	/**
 	 * 
@@ -86,14 +97,38 @@ public class FlightLocator {
 	}
 	
 	/**
-	 * Read and collect flight data from data file.
+	 * Read and collect flight data from index files.
 	 */
 	private void startUp() {
-		
+		try {
+			theFile = new RandomAccessFile("flights.dat", "r");
+			
+			/* Read index file and add flight numbers and offsets to datumMock. */
+			flightData = new ListMap<Integer, FlightRecord>();
+			flightNum = new RandomAccessFile("flightNum.idx", "r");
+			while (flightNum.getFilePointer() < flightNum.length()) {
+				flightData.add(flightNum.readInt(), new FlightRecord(flightNum.readLong(), theFile));
+			}
+			
+			/* Read index file and add departure times and offsets to datumMock. */
+			departData = new ListMap<Long, FlightRecord>();
+			departTime = new RandomAccessFile("departTime.idx", "r");
+			while (departTime.getFilePointer() < departTime.length()) {
+				departData.add(departTime.readLong(), new FlightRecord(departTime.readLong(), theFile));
+			}
+			
+			
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		flightNum.close();
+		departTime.close();
+		destination.close();
 	}
 	
 	/**
-	 * Write updated flight data to data file.
+	 * Write updated flight data to index files.
 	 */
 	private void shutDown() {
 		
